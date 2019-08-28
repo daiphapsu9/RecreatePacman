de4b791fb9c769797b6113b4ba1c02adfce95280
daiphapsu0811 <magician0811@gmail.com>
2019-08-28 22:13:54 +1000

    finish character animation

---
+++ Pacman/Assets/Scripts/Pacman.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacman : MonoBehaviour
+{
+    public float speed;
+    public Animator animator;
+    Vector2 dest = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        animator.SetFloat("DirX", movement.x);
+        animator.SetFloat("DirY", movement.y);
+        animator.SetFloat("PreX", movement.x);
+        animator.SetFloat("PreY", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+        if (Mathf.Abs(movement.x) + Mathf.Abs(movement.y) >= 2f)
+        {
+            movement.x *= (float)System.Math.Sqrt(0.5);
+            movement.y *= (float)System.Math.Sqrt(0.5);
+        }
+
+        transform.position = transform.position + movement * speed * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
